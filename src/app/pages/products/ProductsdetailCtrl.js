41566bee2932c822c56e782bd7e743368eaacf49
Robert Zeng <jianze_zeng@foxitsoftware.cn>
2018-04-22 20:36:38 +0800

    prepare attribute

--- src/app/pages/products/ProductsdetailCtrl.js
+++ src/app/pages/products/ProductsdetailCtrl.js
@@ -33,13 +33,11 @@
 
 		$scope.ready = function(editor) { //编辑器加载完毕
 			editor.execCommand('serverparam', function(editor) {
-					return {
-						'auth': auth					
-				}
+				return {
+					'auth': auth
+				};
 			});
-		}
-
-
+		};
 
 		uploader.filters.push({
 			name: 'imageFilter',
@@ -89,18 +87,74 @@
 				console.log(status);
 				$scope.detail.full_products_image = response.data.url;
 				$scope.detail.products_image = response.data.path;
-				uploader.clearQueue(); //清除上传队列
-				uploader.showtable = false;
+				
 			} else {
 				console.log('error');
 				layer.alert(response.msg);
 			}
 		};
 		uploader.onCompleteAll = function() {
+			uploader.clearQueue(); //清除上传队列
+				uploader.showtable = false;
 			console.info('onCompleteAll');
 		};
 
-		console.info('uploader', uploader);
+		//console.info('uploader', uploader);
+		$scope.showimagelist = false;
+		//处理image list
+		if ($scope.detail.image_list) { //如果图片存在
+			$scope.showimagelist = true;
+
+		}
+		$scope.deleteimage = function(idx) {
+			$scope.detail.image_list.splice(idx, 1);
+		};
+		
+		var uploaders = $scope.uploaders = new FileUploader({
+			url: $localStorage.fileapi,
+			alias: 'file',
+			headers: {
+				'Authorization': auth
+			},
+			autoUpload: true
+		});
+		uploaders.showtable = false; //上传产品多图
+		uploaders.filters.push({
+			name: 'imageFilter',
+			fn: function(item /*{File|FileLikeObject}*/ , options) {
+				var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+				return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+			}
+		});
+		uploaders.onAfterAddingFile = function(fileItem) {
+			//console.info('onAfterAddingFile', fileItem);
+			uploaders.showtable = true;
+		};
+		uploaders.onCompleteItem = function(fileItem, response, status, headers) {
+			console.info('onCompleteItem', fileItem, response, status, headers);
+			console.log(status);
+			if (response.status) {
+				var sizes=$scope.detail.image_list.length+1;
+				var nimages=[];
+				nimages.fullurl=response.data.url;
+				nimages.image=response.data.path;
+				nimages.num=sizes;
+				//nimages.image=response.data;
+				$scope.detail.image_list.push(nimages);
+			} else {
+				
+				layer.alert(response.msg);
+			}
+		};
+	uploaders.onCompleteAll = function() {
+		uploaders.clearQueue(); //清除上传队列
+				uploaders.showtable = false;
+			console.info('onCompleteAll');
+		};
+
+
+
+
 
 	}
 
