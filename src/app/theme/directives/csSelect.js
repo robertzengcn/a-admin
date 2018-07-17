3b742c780f5d20a57c03fd442735de4e428bface
Robert Zeng <jianze_zeng@foxitsoftware.cn>
2018-07-17 17:40:06 +0800

    add checkbox

---
+++ src/app/theme/directives/csSelect.js
@@ -0,0 +1,33 @@
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.theme')
+      .directive('csSelect', csSelect);
+
+      function csSelect(){
+      return {
+        require: '^stTable',
+        template: '<input type="checkbox"/>',
+        scope: {
+            row: '=csSelect'
+        },
+        link: function (scope, element, attr, ctrl) {
+
+            element.bind('change', function (evt) {
+                scope.$apply(function () {
+                    ctrl.select(scope.row, 'multiple');
+                });
+            });
+
+            scope.$watch('row.isSelected', function (newValue, oldValue) {
+                if (newValue === true) {
+                    element.parent().addClass('st-selected');
+                } else {
+                    element.parent().removeClass('st-selected');
+                }
+            });
+        }
+    };	
+      }
+
+})();
