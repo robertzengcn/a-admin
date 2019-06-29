34e7cbaa57e3c72774a22102073096cff383b1e9
robert hanjiang <1093968009@qq.com>
2019-06-29 22:02:41 +0800

    update confie

--- src/jslib/ueditor/ueditor.all.js
+++ src/jslib/ueditor/ueditor.all.js
@@ -24582,6 +24582,58 @@ UE.plugin.register('simpleupload', function (){
                 domUtils.on(iframe, 'load', callback);
                 form.action = utils.formatUrl(imageActionUrl + (imageActionUrl.indexOf('?') == -1 ? '?':'&') + params);
                 form.submit();
+                
+//                 var formdata = new FormData(form);
+// var arr,reg=new RegExp("(^| )_token=([^;]*)(;|$)");
+
+// var myForm = document.getElementById("myForm");
+// var xhr= new XMLHttpRequest();
+// var auth= me.getOpt('auth');
+// xhr.open("POST", me.getOpt('serverUrl')+'?actions=uploadimage&auth='+auth, true);
+// xhr.onreadystatechange = function() {
+//     if (xhr.readyState === 4)
+//         if ((xhr.status >=200 && xhr.status < 300) || xhr.status == 304)
+//             alert(xhr.responseText);
+// }
+// xhr.send(formdata);
+
+// xhr.onreadystatechange = function () {
+//     if(xhr.readyState == 4) {
+//         console.log(xhr.responseText);
+//         var response = JSON.parse(xhr.responseText);
+//         if(response.state =='SUCCESS'){
+//             loader = me.document.getElementById(loadingId);
+//             loader.setAttribute('src', me.options.imageUrlPrefix+response.url);
+//             loader.setAttribute('_src', me.options.imageUrlPrefix+response.url);
+//             loader.setAttribute('title', response.title || '');
+//             loader.setAttribute('alt', response.original || '');
+//             loader.removeAttribute('id');
+//             domUtils.removeClasses(loader, 'loadingclass');
+//         }else
+//         {
+//             showErrorLoader && showErrorLoader(response.state);
+//         }
+//     }
+// }
+
+// xhr.onreadystatechange = function () {
+//     if(xhr.readyState == 4) {
+//         console.log(xhr.responseText);
+//         var response = JSON.parse(xhr.responseText);
+//         if(response.state =='SUCCESS'){
+//             loader = me.document.getElementById(loadingId);
+//             loader.setAttribute('src', response.url);
+//             loader.setAttribute('_src', response.url);
+//             loader.setAttribute('title', response.title || '');
+//             loader.setAttribute('alt', response.original || '');
+//             loader.removeAttribute('id');
+//             domUtils.removeClasses(loader, 'loadingclass');
+//         }else
+//         {
+//             showErrorLoader && showErrorLoader(response.state);
+//         }
+//     }
+// }
             });
 
             var stateTimer;
